d8422179b83ed03f17f79b84cb9d0b9efdb1f9f3
Tyler Ryan <tlrrn5@gmail.com>
2023-02-15 16:40:04 -0600

    Updated xdo support for Fedora package manager
    toggle_win_vis.sh only supported xdo. However, Fedora calls xdo "xdotool".
    The script didn't account for it. Now it does.
    
    Additionally, it should be relatively easy to add support for other package
    manager that call xdo something else now that I've added the case statement.

--- scripts/toggle_win_vis.sh
+++ scripts/toggle_win_vis.sh
@@ -4,23 +4,49 @@
 # of a single window for BSPWM
 
 # Current window location in memory
-export win_id=$(xdo id)
+# This should work with Pacman and Dnf
+
 TMP_FILE=/tmp/window-id.txt
+
+xdo_tool=$(ls /usr/bin | grep ^xdo)
+[[ $? -eq 1 ]] && notify-send "error: you don't have `xdo` installed."
+
+export WIN_ID=$(xdo id)
+HIDE_COMMAND="xdo hide"
+SHOW_COMMAND="xdo show"
+
+function set_commands()
+{
+    # Use of case statement in case other package managers
+    # call xdo something different.
+    case ${xdo_tool} in
+        "xdotool")
+            export WIN_ID=$(xdotool getactivewindow)
+            HIDE_COMMAND="xdotool windowunmap"
+            SHOW_COMMAND="xdotool windowmap"
+            ;;
+        *)
+            notify-send "error: $?"
+            exit 1
+            ;;
+    esac
+}
 
 hide_window()
 {
     # Put that location in a temp file.
-    echo $win_id > $TMP_FILE
+    echo ${WIN_ID} > ${TMP_FILE}
     # hide the window
-    xdo hide $win_id
+    ${HIDE_COMMAND} ${WIN_ID}
 }
 
 show_window()
 {
     # Find the id of the hidden window
     hidden_win_id=$(cat ${TMP_FILE})
-    xdo show $hidden_win_id
-    rm $TMP_FILE
+    # show the hidden window
+    ${SHOW_COMMAND} ${hidden_win_id}
+    rm ${TMP_FILE}
 }
 
 toggle_window()
@@ -32,5 +58,7 @@ toggle_window()
     fi
 }
 
+set_commands
 toggle_window
+
 exit 0

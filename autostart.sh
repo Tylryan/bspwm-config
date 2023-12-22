8d1476eadd58d37842ddda6751603b8c3871700d
Tyler Ryan <tlrrn5@gmail.com>
2023-12-21 23:43:11 -0600

    able to work with multiple monitors

--- autostart.sh
+++ autostart.sh
@@ -7,8 +7,10 @@ function run {
   fi
 }
 
+setxkbmap -option caps:swapescape                    # Swap caps with escape key
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-xrandr --output LVDS1 --primary --mode 1600x900 --rotate normal --output VGA1 --mode 1920x1080  --rotate normal --right-of LVDS1
+#xrandr --output LVDS1 --primary --mode 1600x900 --rotate normal --output VGA1 --mode 1920x1080  --rotate normal --right-of LVDS1
+xrandr --output LVDS-1 --primary --mode 1600x900 --rotate normal --output HDMI-3 --mode 1920x1080  --rotate normal --left-of LVDS-1
 
 $HOME/.config/polybar/launch.sh &
 
@@ -20,9 +22,10 @@ run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 feh --bg-scale ~/.config/bspwm/wallpaper
 
 xsetroot -cursor_name left_ptr &
+lxappearance &
+
 
 numlockx on &
 #picom --config $HOME/.config/bspwm/picom.conf &      # Window transitions and transparency
 exec picom -b
 wmname LG3D &
-setxkbmap -option caps:swapescape                    # Swap caps with escape key

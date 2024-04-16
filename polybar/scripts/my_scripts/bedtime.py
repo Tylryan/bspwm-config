8862ad459ff483772c326cfe2f38aac46b23babb
Tyler Ryan <tlrrn5@gmail.com>
2024-04-15 21:21:24 -0500

    maybe the bedtime script is correct now????

--- polybar/scripts/my_scripts/bedtime.py
+++ polybar/scripts/my_scripts/bedtime.py
@@ -7,9 +7,12 @@ bedtime_hour   = 23
 bedtime_minute = 0
 
 hours_til_bed   = bedtime_hour - current_hour
-minutes_til_bed = 60 - bedtime_minute - current_minute
+minutes_til_bed = bedtime_minute - current_minute
 
-if   minutes_til_bed <= 0: minutes_til_bed = "00"
+if minutes_til_bed <= 0: 
+    minutes_til_bed %= 60
+    hours_til_bed-=1
+
 elif minutes_til_bed < 10: minutes_til_bed = f"0{minutes_til_bed}"
 
 if hours_til_bed < 0 : 
